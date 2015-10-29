e13dd78239c32a99d93d5153feafeaf090a36d15
brauni91 <oliver.braunsdorf@gmx.de>
2015-10-29 01:04:14 +0100

    - added latex files for manual

--- src/pbcVersion.h
+++ src/pbcVersion.h
@@ -44,8 +44,8 @@ class PBCVersion {
  public:
     static const unsigned int MAJOR = 0;
     static const unsigned int MINOR = 4;
-    static const unsigned int REVISION = 44;
-    static const unsigned int BUILD = 150910887;
+    static const unsigned int REVISION = 48;
+    static const unsigned int BUILD = 1510281090;
 
 	/**
     * @brief compares the string of the current version to another version string
