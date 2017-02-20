83253ac53e46c38c6efca211ca1e0d3dddc6ad02
brauni91 <oliver.braunsdorf@gmx.de>
2017-02-20 03:34:49 +0100

    - added missing files from last commit

---
+++ doc/html/search/classes_3.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['returnstruct',['ReturnStruct',['../structPBCExportPDFDialog_1_1ReturnStruct.html',1,'PBCExportPDFDialog']]],
+  ['returnstruct',['ReturnStruct',['../structPBCOpenPlayDialog_1_1ReturnStruct.html',1,'PBCOpenPlayDialog']]],
+  ['returnstruct',['ReturnStruct',['../structPBCNewPlaybookDialog_1_1ReturnStruct.html',1,'PBCNewPlaybookDialog']]],
+  ['returnstruct',['ReturnStruct',['../structPBCNewPlayDialog_1_1ReturnStruct.html',1,'PBCNewPlayDialog']]],
+  ['returnstruct',['ReturnStruct',['../structPBCSavePlayAsDialog_1_1ReturnStruct.html',1,'PBCSavePlayAsDialog']]]
+];
---
+++ doc/html/search/defines_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['pbcassert',['pbcAssert',['../pbcDeclarations_8h.html#a1b6fbd94d36b9a357296fd2d7faf3873',1,'pbcDeclarations.h']]]
+];
