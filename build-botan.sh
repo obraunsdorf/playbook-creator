a374cfdf166319a9181a5eca1b47333820857a20
Oliver Braunsdorf <oliver.braunsdorf@aisec.fraunhofer.de>
2020-03-24 15:00:06 +0100

    build-botan.sh

--- build-botan.sh
+++ build-botan.sh
@@ -1,9 +1,9 @@
 if [[ ! -f botan/libbotan-2.a ]]; then
 	echo "hallo"
-#	git clone https://github.com/randombit/botan botan;
-#	cd botan;
-#	git checkout 2.6.0;
-#	python configure.py --disable-shared-library --disable-modules=darwin_secrandom;
-#	make;
-#	cd ..;
+	git clone https://github.com/randombit/botan botan;
+	cd botan;
+	git checkout 2.6.0;
+	python configure.py --disable-shared-library --disable-modules=darwin_secrandom;
+	make;
+	cd ..;
 fi
