f42f92750598f5e88981cce58a91f32d0622ea99
Oliver Braunsdorf <oliver.braunsdorf@gmx.de>
2020-03-25 01:33:55 +0100

    cache boost directory

--- build-boost.sh
+++ build-boost.sh
@@ -3,7 +3,7 @@ if [[ ! -f boost/lib/libboost_serialization.a ]]; then
 	curl -O -L https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.gz;
 	tar -xzf boost_1_66_0.tar.gz;
 	cd boost_1_66_0;
-	./bootstrap.sh --with-libraries=serialization,filesystem,test --prefix=/usr/local/opt
+	./bootstrap.sh --with-libraries=serialization,filesystem,test --prefix=../boost
 	./b2 install link=static runtime-link=static &>/dev/null
 	cd ..;
 fi
