5a56c654a3a8f648e4ec282417448df1b745a51f
Oliver Braunsdorf <oliver.braunsdorf@aisec.fraunhofer.de>
2021-08-02 01:13:01 +0200

    Disable update checking on MacOS (see issue #33)

--- src/main.cpp
+++ src/main.cpp
@@ -49,12 +49,15 @@ int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainDialog w;
     try {
+        #if TARGET_OS_MAC
+            std::cout << "Update checking has been disabled on MacOS (see https://github.com/obraunsdorf/playbook-creator/issues/33) " << std::endl; 
+        #else
         std::cout << "Looking for updates. This should only take a few seconds. Please wait..." << std::endl;
         const uint32_t BUFF_LEN = 500;
         uint8_t buffer[BUFF_LEN] = {0};
         CBuffer desc_result_buf = CBuffer{buffer, BUFF_LEN};
-        UpdateCheckingStatus ucs = updates_available(PBC_VERSION_MAJOR, PBC_VERSION_MINOR, PBC_VERSION_PATCH, &desc_result_buf); // TODO: numeric conversions might be problematic here!
         w.show();
+        UpdateCheckingStatus ucs = updates_available(PBC_VERSION_MAJOR, PBC_VERSION_MINOR, PBC_VERSION_PATCH, &desc_result_buf); // TODO: numeric conversions might be problematic here!
         switch (ucs.tag) {
             case UpdateCheckingStatus::Tag::UpdatesAvailable:
                 {
@@ -91,6 +94,7 @@ int main(int argc, char *argv[]) {
                 break;
 
         }
+        #endif //TARGET_OS_MAC
         a.exec();
     } catch(std::exception &e) {
         QString errorString = "Terminating playbook creator. Please open an bug report on "
