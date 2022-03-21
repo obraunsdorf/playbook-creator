a733001c93793e9dcf7baa0eb5b930931beb05fd
Oliver Braunsdorf <oliver.braunsdorf@aisec.fraunhofer.de>
2022-03-21 10:36:12 +0100

    cxx static CRT

--- rust-helpers/build.rs
+++ rust-helpers/build.rs
@@ -2,5 +2,6 @@ fn main() {
     cxx_build::bridge("src/lib.rs")
         .compiler("clang")
         .flag_if_supported("-std=c++11")
+        .static_crt(true)  // link to MSVCRT statically on Windows (only used on windows)
         .compile("cxx-demo");
 }
