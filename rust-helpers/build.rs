6f685406252c12d4a16d41573183ce727a5062d0
Oliver Braunsdorf <oliver.braunsdorf@aisec.fraunhofer.de>
2022-03-21 11:27:03 +0100

    cxx -static flag

--- rust-helpers/build.rs
+++ rust-helpers/build.rs
@@ -1,7 +1,14 @@
 fn main() {
-    cxx_build::bridge("src/lib.rs")
+    let mut cxx_build = cxx_build::bridge("src/lib.rs");
+
+    cxx_build
         .compiler("clang")
         .flag_if_supported("-std=c++11")
-        .static_crt(true)  // link to MSVCRT statically on Windows (only used on windows)
-        .compile("cxx-demo");
+        .static_crt(true); // link to MSVCRT statically on Windows (only used on windows)
+
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        cxx_build.static_flag(true);
+    }
+
+    cxx_build.compile("cxx-demo");
 }
