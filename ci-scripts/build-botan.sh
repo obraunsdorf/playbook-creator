5daf0bea61ed44bd18e5446b346e005dce45a298
Oliver Braunsdorf <oliver.braunsdorf@gmx.de>
2026-02-18 19:31:58 +0100

    Update dependencies and build systems for different Operating systems (#47)
    * update devcontainer to ubuntu24.04 and update dependencies
    
    * update CI file for Linux
    
    * CI: don't fail fast in matrix builds
    
    * install botan via apt but force linking to static library
    
    * GUI forwarding via X11
    
    * Windows CI: use pinned vcpkg
    
    * MacOS CI: fix version in cmake dependency script
    
    * update botan to 2.19.5
    
    * upgrade dependencies of pbcUpdater
    
    * better integration of Rust and Cargo into CMake via Corrosion
    
    * support macos on intel and arm

--- ci-scripts/build-botan.sh
+++ ci-scripts/build-botan.sh
@@ -2,7 +2,7 @@ if [[ ! -f botan/libbotan-2.a ]]; then
 	echo "hallo"
 	git clone https://github.com/randombit/botan botan;
 	cd botan;
-	git checkout 2.6.0;
+	git checkout 2.19.5;
 	python configure.py --disable-shared-library --disable-modules=darwin_secrandom;
 	make;
 	cd ..;
